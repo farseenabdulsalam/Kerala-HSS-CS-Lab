09dd80598d67e92e997e06abc453af8eaab39207
Farzeen <farseenabdulsalam@gmail.com>
2016-01-18 09:22:55 +0530

    Moved PDF/src to extra-src. Built first successful build of makeit

---
+++ js/build-files/it-fair-validation/input.js
@@ -0,0 +1,4 @@
+document.getElementsByName('school_name')[0].value='KPC HSS'
+document.getElementsByName('user_name')[0].value='User'
+document.getElementsByName('pw')[0].value='Password'
+document.getElementsByName('mobile')[0].value='9876543210'
